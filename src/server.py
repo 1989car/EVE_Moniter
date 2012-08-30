1b02d0bbc8c59a07d3193952cb2a83e0b7d00a7f
Oishi <chehaojia@gmail.com>
2012-08-30 23:54:29 +0800

    生成签名图功能完成~~~~~~~~~~

--- src/server.py
+++ src/server.py
@@ -5,86 +5,40 @@ Created on 2012-8-28
 
 @author: Administrator
 '''
-import re
-import socket
-import time  
+import time
 
 import web
+
+import serverImage
+
 urls = (
   "", "redirect",
-  "/","ServerStat"
+  "/*","Server",
+
 )
 
 class redirect:
     def GET(self): raise web.seeother('/')
 
 
-class ServerStat:
+class Server:
     '''
     Server Stat
     '''
-    startr = re.compile('Starting\ up\.\.\.\((\d+)\ sec\.\)')
-
-    HOST='211.144.214.68'
-    PORT=26000
-
-    dat = ''.join([ chr(x) for x in [0x23, 0x00, 0x00, 0x00, 0x7E, 0x00, 0x00, 0x00,
-                        0x00, 0x14, 0x06, 0x04, 0xE8, 0x99, 0x02, 0x00,
-                        0x05, 0x8B, 0x00, 0x08, 0x0A, 0xCD, 0xCC, 0xCC,
-                        0xCC, 0xCC, 0xCC, 0x00, 0x40, 0x05, 0x49, 0x0F,
-                        0x10, 0x05, 0x42, 0x6C, 0x6F, 0x6F, 0x64 ]])
-    
-    def time(self):
-        currentTime =  time.strftime("%Y年%m月%d日 %H:%M:%S",time.localtime())
-        print currentTime
-        return currentTime
-        
-    def getNum(self, cin ):
-        return sum([ ord(n) * ( 256 ** p )  for (n ,p )  in zip( cin , range(4))]) 
-    
-    def getServer(self):
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((self.HOST, self.PORT))
-            s.send(self.dat )
-            idat =  s.recv( 4096 )       
-            
-            if self.startr.search( idat ):
-                m = self.startr.search( idat )
-                '''print 'Starting:%s' % (m.groups(1)[0])'''
-                return -99
-            else:        
-                v = ord( idat[19] )
-                if v == 4:
-                    cnt = self.getNum( idat[20:25] )
-                elif v == 5:
-                    cnt = self.getNum(  idat[20:22])
-                elif v == 6:
-                    cnt = self.getNum( idat[20])
-                else:
-                    cnt = 0
-                
-                '''print 'EVE Server Online:%u' % ( cnt )'''
-                return cnt
-        except:
-                '''return "Offline"'''
-                return -1 
+    waittime = 1
                 
     def GET(self):
-        print self.getServer()
+        '''
         render = web.template.render("templates")
-        if self.getServer()==-1:
-            player = 0
-            stats = u"离线"
-        elif self.getServer()==-99:
-            player = 0
-            stats = u"正在启动"
-        else:
-            player = self.getServer()
-            stats = u"在线"
-            
-        return render.server(player,stats,self.time())
         
-
-
+        return render.server()
+        '''
+        currenttime = int(time.time())
+        if currenttime%self.waittime == 0:
+            '''web.header("Content-type","image/x-png")'''
+            return serverImage.createImage()
+        else:
+            web.notmodified()
+       
+     
 app_server = web.application(urls, locals())
---
+++ src/serverImage.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# coding: utf-8
+'''
+Created on 2012-8-30
+
+@author: Administrator
+'''
+import os,time,re,socket,StringIO
+
+import Image, ImageDraw,ImageFont 
+
+
+startr = re.compile('Starting\ up\.\.\.\((\d+)\ sec\.\)')
+
+HOST='211.144.214.68'
+PORT=26000
+
+dat = ''.join([ chr(x) for x in [0x23, 0x00, 0x00, 0x00, 0x7E, 0x00, 0x00, 0x00,
+                        0x00, 0x14, 0x06, 0x04, 0xE8, 0x99, 0x02, 0x00,
+                        0x05, 0x8B, 0x00, 0x08, 0x0A, 0xCD, 0xCC, 0xCC,
+                        0xCC, 0xCC, 0xCC, 0x00, 0x40, 0x05, 0x49, 0x0F,
+                        0x10, 0x05, 0x42, 0x6C, 0x6F, 0x6F, 0x64 ]])
+    
+def getTime():
+        Time =  time.strftime("UpdateTime %Y-%m-%d  %H:%M:%S",time.localtime())
+        '''print Time'''
+        return Time
+        
+def getNum(cin ):
+        return sum([ ord(n) * ( 256 ** p )  for (n ,p )  in zip( cin , range(4))]) 
+    
+def getServer():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((HOST, PORT))
+        s.send(dat )
+        idat =  s.recv( 4096 )       
+            
+        if startr.search( idat ):
+            '''m = startr.search( idat )           
+            print 'Starting:%s' % (m.groups(1)[0])'''
+            return -99
+            
+        else:        
+            v = ord( idat[19] )
+            if v == 4:
+                cnt = getNum( idat[20:25] )
+            elif v == 5:
+                cnt = getNum(  idat[20:22])
+            elif v == 6:
+                cnt = getNum( idat[20])
+            else:
+                cnt = 0
+                
+            '''print 'EVE Server Online:%u' % ( cnt )'''
+            return cnt
+    except:
+            '''return "Offline"'''
+            return -1 
+
+def createImage():
+    img = Image.open(os.path.join("static", "server.png"))
+    
+    if getServer()==-1:
+            player = str(0)
+            stats = u"OFFLINE"
+    elif getServer()==-99:
+            player = str(0)
+            stats = u"starting"
+    else:
+            player = str(getServer() )
+            stats = u"ONLINE"
+    
+    
+    ctime = unicode( getTime(),"utf-8") 
+    
+    
+    '''print ctime'''
+    
+    sign = unicode("暗月重工")
+    
+    imgdraw = ImageDraw.Draw(img)
+    
+    font = ImageFont.truetype( os.path.join("static", "MT.ttf"), 28) 
+    timefont = ImageFont.truetype( os.path.join("static", "MT.ttf"), 12) 
+    signfont = ImageFont.truetype( os.path.join("static", "sign.ttf"),16)
+    
+    '''print img.format, img.size, img.mode'''
+    
+    imgdraw.text( (20,8),"Server",font=font,fill="#FFFFFF")
+    
+    imgdraw.text( (50, 40), stats, font=font, fill="#FFFFFF" )
+    
+    imgdraw.text( (420,8),"Player",font=font,fill="#FFFFFF")
+
+    imgdraw.text( (470, 40), player, font=font, fill="#FFFFFF" )
+    
+    imgdraw.text( (20, 118), sign, font=signfont, fill="#FFFFFF" )
+    
+    imgdraw.text( (350, 118), ctime, font=timefont, fill="#FFFFFF" )
+    
+    '''
+    img.show()
+    '''
+    '''img.save(os.path.join("static","output.png"))'''
+    
+    buff = StringIO.StringIO()
+    
+    img.save(buff,"png")
+    '''buff.close()'''
+    '''img.save("stats.png")'''
+    return buff.getvalue()
+    
